31d71af5c51d0c998599d6cbebcbc439d5b1b636
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-12 20:13:05 -0600

    agregue diagramas de feyman

--- animaciones.py
+++ animaciones.py
@@ -1,2 +1,14 @@
 import streamlit as st
 st.title ("Particulas elementales")
+from sympy import *
+import heppackv0 as hep
+s,theta=symbols('s theta',real=True)
+p=symbols('p',positive=True)
+u,t,s=symbols('u t s',real=True)
+p1=[p,0,0,0]
+p2=[p,0,pi,pi]
+p3=[p,0,theta,0]
+p4=[p,0,pi-theta,pi]
+sCM=4*p*p;sCM
+tCM=-sCM*sin(theta/2)**2;tCM
+uCM=-sCM*cos(theta/2)**2;uCM
