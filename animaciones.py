5290970cf1884b4d5c96baa7a3707f880b8e074a
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-14 20:17:25 -0600

    definición

--- animaciones.py
+++ animaciones.py
@@ -1,3 +1,4 @@
 import streamlit as st
-st.title ("Particulas elementales")
-st.header ("Esta aplicación te eneseñara un poco acerca de las partículas elementales.") 
+st.title ("Partículas elementales")
+st.header ("Esta aplicación te enseñara un poco acerca de las partículas elementales.") 
+st.write ("Las partículas elementales podrían considerarse como los ladrillos básicos de la creación, se les llama partículas elementales ya que no están constituidas por partículas más pequeñas.")
