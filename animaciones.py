36be7b2ac3f275f5b1ba3706866ebd615921ad5a
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-14 20:35:07 -0600

    Update animaciones.py

--- animaciones.py
+++ animaciones.py
@@ -4,7 +4,6 @@ st.header ("Esta aplicación te enseñara un poco acerca de las partículas elem
 st.write ("Las partículas elementales podrían considerarse como los ladrillos básicos de la creación, se les llama partículas elementales ya que no están constituidas por partículas más pequeñas.")
 if st.button ("Quarks"):
   st.write ("Son responsables de la formación y estructura de los núcleos atómicos y de las interacciones con su entorno.")
- else:
 st.button ("Leptones")
 st.button ("Bosón de gauge")
 st.button ("Bosón de Higgs")
