90272ef46f8c4f13544eec932926a06bce0cf263
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-14 21:20:41 -0600

    Update animaciones.py

--- animaciones.py
+++ animaciones.py
@@ -2,8 +2,10 @@ import streamlit as st
 st.title ("Partículas elementales")
 st.header ("Esta aplicación te enseñara un poco acerca de las partículas elementales.") 
 st.write ("Las partículas elementales podrían considerarse como los ladrillos básicos de la creación, se les llama partículas elementales ya que no están constituidas por partículas más pequeñas.")
-if on_click.button ("Quarks"):
-  st.write ("Son responsables de la formación y estructura de los núcleos atómicos y de las interacciones con su entorno.") 
+if st.button ("Quarks"):
+  st.write ("Son responsables de la formación y estructura de los núcleos atómicos y de las interacciones con su entorno.")
+  st.print (https://www.google.com/url?sa=i&url=https%3A%2F%2Fmakeagif.com%2Fgif%2Fatom-into-quarks-LXv7Y3&psig=AOvVaw0b55skzQJc3UCEbibkYADM&ust=1650075760658000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJD4k6-BlfcCFQAAAAAdAAAAABAz 
+           )
 st.button ("Leptones")
 st.button ("Bosón de gauge")
 st.button ("Bosón de Higgs")
