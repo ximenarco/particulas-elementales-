53ca5790c46aa8d79c77b8a46beb9a3f491b464a
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-14 20:10:49 -0600

    Update animaciones.py

--- animaciones.py
+++ animaciones.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title ("Particulas elementales")
-st.output ("Esta aplicación te eneseñara un poco acerca de las partículas elementales") 
+st.str ("Esta aplicación te eneseñara un poco acerca de las partículas elementales") 
