325d31507e94fd205c61aeaad022e1d79e7e74e9
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-22 19:23:24 -0600

    Update animaciones.py

--- animaciones.py
+++ animaciones.py
@@ -1,7 +1,8 @@
 import streamlit as st
-st.title("Partículas elementales")
-st.header("Esta aplicación te enseñara un poco acerca de las partículas elementales.") 
-st.write("Las partículas elementales podrían considerarse como los ladrillos básicos de la creación, se les llama partículas elementales ya que no están constituidas por partículas más pequeñas.")
+if st.button:
+  st.title("Partículas elementales")
+  st.header("Esta aplicación te enseñara un poco acerca de las partículas elementales.") 
+  st.write("Las partículas elementales podrían considerarse como los ladrillos básicos de la creación, se les llama partículas elementales ya que no están constituidas por partículas más pequeñas.")
 
 with st.sidebar:
   bt1=st.button("Quarks")
