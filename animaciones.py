8bfb2e7c2c8bc6657e61006e45d367f18a08b184
Ochoa2in <102831684+Ochoa2in@users.noreply.github.com>
2022-04-22 20:38:39 -0600

    Update animaciones.py

--- animaciones.py
+++ animaciones.py
@@ -12,6 +12,7 @@ with st.sidebar:
   bt4=st.button("Bosón de Higgs")
   
 if bt1:
+  st.header("Quarks")
   st.write("Son responsables de la formación y estructura de los núcleos atómicos y de las interacciones con su entorno.")
   st.write("Los quarks son un tipo de partícula que constituye la materia. Si miramos a nuestro alrededor... toda la materia que vemos está compuesta de protones y neutrones, y estas partículas están compuestas de quarks.")
   col1,col2= st.columns (2)
@@ -33,12 +34,15 @@ if bt1:
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Neutron_quark_structure.svg/1200px-Neutron_quark_structure.svg.png")
     
 if bt2:
+  st.header("Leptones")
   st.write("Lepton, cualquier miembro de una clase de partículas subatómicas que responden solo a la fuerza electromagnética, la fuerza débil y la fuerza gravitacional y no se ven afectadas por la fuerza fuerte. Se dice que los leptones son partículas elementales; es decir, no parecen estar formados por unidades de materia más pequeñas. Los leptones pueden transportar una unidad de carga eléctrica o ser neutrales.")
   st.write("Los leptones son partículas elementales con espín 1/2 (un fermión) que no se ven afectadas por la fuerza nuclear fuerte. Son una familia de partículas diferente a la otra familia conocida de fermiones, los quarks. Los electrones son un ejemplo bien conocido que se encuentran en la materia ordinaria.")
   
 if bt3:
+  st.header("Bosón de gauge")
   st.write("un bosón que media la interacción entre partículas elementales. Hay varios tipos: fotones para interacciones electromagnéticas, bosones vectoriales intermedios W y Z para interacciones débiles y gravitones para interacciones gravitatorias.")
   
 if bt4:
+  st.header("Bosón de Higgs")
   st.write("Los bosones a veces se denominan partículas de fuerza, porque son los bosones los que controlan la interacción de las fuerzas físicas, como el electromagnetismo y posiblemente incluso la propia gravedad.")
  
